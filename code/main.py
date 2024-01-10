262f387359ecb4c548239a109ce3cf1ecc464644
Rafal Ozog <rafalozog@gmail.com>
2024-01-10 03:38:47 +0000

    First Unit Tests implemented. Inspiration of TDD

--- code/main.py
+++ code/main.py
@@ -1,5 +1,8 @@
 from simulation_logic.strategy_simulation import run_multiple_simulations
 from config import COMPANIES_SET, START_DATE, END_DATE, ATTRIBUTE_OF_DECISION_INDEX, SUB_PERIOD_LENGTH_IN_DAYS_ARRAY
+import unittest
+from simulation_logic.calculation_utils import TestCalculateChangeInSharePrice
 
 # Main App
-run_multiple_simulations(COMPANIES_SET, START_DATE, END_DATE, ATTRIBUTE_OF_DECISION_INDEX, None, SUB_PERIOD_LENGTH_IN_DAYS_ARRAY)
+unittest.main() # Run Unit Tests
+# run_multiple_simulations(COMPANIES_SET, START_DATE, END_DATE, ATTRIBUTE_OF_DECISION_INDEX, None, SUB_PERIOD_LENGTH_IN_DAYS_ARRAY)
