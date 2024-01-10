7d5c30b2d8c0a07ee18450b52e00ac49f4fd7ac9
Rafal Ozog <rafalozog@gmail.com>
2024-01-10 08:28:58 +0000

    Code cleaning: removal of unused code

--- code/simulation_logic/weights_factory.py
+++ code/simulation_logic/weights_factory.py
@@ -25,19 +25,11 @@ def get_weights_for_bets_for_given_companies_for_given_date(companies, attribute
     average_weight = sum_of_weights / len(calculated_weights)
     recalculated_weights = []
     sum_of_recalculated_weights = 0.0
-    for weight in calculated_weights:
-        company_ticker = weight[0]
-        weight_value = weight[1]
-        # Only positive numbers
+    for company_ticker, weight_value in calculated_weights:
         recalculated_weight = weight_value - average_weight
         recalculated_weights.append((company_ticker, recalculated_weight))
-        if recalculated_weight > 0:
-            sum_of_recalculated_weights += recalculated_weight
-        if recalculated_weight < 0:
-            sum_of_recalculated_weights += (recalculated_weight * -1)
-    for weight in recalculated_weights:
-        company_ticker = weight[0]
-        weight_value = weight[1]
+        sum_of_recalculated_weights += abs(recalculated_weight) # Only positive numbers
+    for company_ticker, weight_value in recalculated_weights:
         normalized_weight = weight_value / sum_of_recalculated_weights
         normalized_weights.append((company_ticker, normalized_weight))
     return normalized_weights
@@ -46,23 +38,29 @@ def _get_investment_value_for_given_company_for_given_date_n_periods(number_of_p
     financial_reports = get_most_recent_financial_reports_for_given_company_until_particular_date(number_of_periods, company_ticker, date)
     if financial_reports == []:
         raise ValueError("ERROR: Empty list")
+
     average_attribute_of_decision_value = 0.0
+    index_of_amount_of_shares = 4
     for report in financial_reports:
-        index_of_amount_of_shares = 4
         decision_value = report[attribute_of_decision_index] / report[index_of_amount_of_shares]
-        average_attribute_of_decision_value = average_attribute_of_decision_value + decision_value
-    average_attribute_of_decision_value = average_attribute_of_decision_value / len(financial_reports)
-    # attributeOfdecision_value = financial_reports[0][attribute_of_decision_index]
-    share_price_for_this_date = None
-    date_variable = date
-    for i in range(0, 5): # I want to try to increase 4 times, on 5th attempt - print error. The loop works in 0 - (n-1) range.
-        share_price_for_this_date = read_db_share_price_in_particular_day(company_ticker, date_variable)
-        if (share_price_for_this_date == None):
-            date_variable = increase_date_by_day(date_variable, DATE_FORMAT)
-        else:
-            break
+        average_attribute_of_decision_value =+ decision_value
+    average_attribute_of_decision_value /= len(financial_reports)
+    share_price_for_this_date = _find_share_price_for_this_date(date, company_ticker)
     investment_value = average_attribute_of_decision_value / share_price_for_this_date
     return investment_value
+
+def _find_share_price_for_this_date(date, company_ticker):
+    share_price_for_this_date = None
+    date_variable = date
+    attempt = 0
+    while share_price_for_this_date is None and attempt < 5:
+        share_price_for_this_date = read_db_share_price_in_particular_day(company_ticker, date_variable)
+        if share_price_for_this_date is None:
+            date_variable = increase_date_by_day(date_variable, DATE_FORMAT)
+        attempt += 1
+    if share_price_for_this_date is None:
+        raise ValueError("ERROR: Could not find share price after 5 attempts.")
+    return share_price_for_this_date
 
 # Unused code (to be researched)
 
