4e9c4717240182d939e7de2b2981bfb353293b63
Rafal Ozog <rafalozog@gmail.com>
2024-01-17 06:18:56 +0000

    Code cleaning: database_utils refactoring

--- code/database_utils.py
+++ code/database_utils.py
@@ -54,13 +54,13 @@ def _save_share_prices_data(data, date_format = DATE_FORMAT):
 
 def find_share_price_for_this_date(date, company_ticker):
     share_price_for_this_date = None
-    date_variable = date
-    attempt = 0
-    while share_price_for_this_date is None and attempt < 5:
-        share_price_for_this_date = _read_db_share_price_in_particular_day(company_ticker, date_variable)
-        if share_price_for_this_date is None:
-            date_variable = increase_date_by_day(date_variable, DATE_FORMAT)
-        attempt += 1
+    for _ in range(5):
+        fetched_share_price_data = fetch_prices_in_particular_period(company_ticker, date, date, True)
+        try:
+            share_price_for_this_date = fetched_share_price_data[0][0]
+            break
+        except IndexError:
+            date = increase_date_by_day(date, DATE_FORMAT)
     if share_price_for_this_date is None:
         raise ValueError("ERROR: Could not find share price after 5 attempts.")
     return share_price_for_this_date
@@ -89,26 +89,6 @@ def fetch_prices_in_particular_period(company, start_date, end_date, is_it_last_
             share_prices_table = _read_share_prices_per_particular_period(company, start_date, end_date_variable)
     return [[row[1] for row in share_prices_table]]
 
-# def _fetch_financial_data_from_google_sheets_csv(file_path):
-#     data_to_save = []
-#     with open(file_path, newline='') as csvfile:
-#         reader = csv.reader(csvfile)
-#         next(reader, None)  # Skip the header row
-#         for row in reader:
-#             if row:
-#                 data_to_save.append([row[0], row[1], 'USD', int(row[2].replace(',', '')), int(row[3].replace(',', ''))]) # Todo: "numbers are in thousands"; hard-coded currency;
-#     return data_to_save
-
-# def _save_financial_data(data):
-#     for report in data:
-#         ticker = report[0]
-#         filling_date_formatted = (datetime.strptime(report[1], "%m/%d/%Y")).strftime("%Y-%m-%d")
-#         currency = report[2]
-#         free_cash_flow = report[3]
-#         amount_of_shares = report[4]
-#         data = (ticker, filling_date_formatted, currency, free_cash_flow, amount_of_shares)
-#         _save_fetched_data_into_database(SQL_QUERY_SAVE_FINANCIALS, data)
-
 def _read_all_data_from_databases():
     queries = {
         "TABLE: SHARE_PRICES": SQL_QUERY_READ_ALL_SHARE_PRICES,
@@ -130,7 +110,3 @@ def _read_share_prices_per_particular_period(company, start_date, end_date):
     with con:
         data = con.execute("SELECT * FROM SHARE_PRICES WHERE ticker = '" + company + "' AND date >='" + start_date + "' AND date <='" + end_date + "'")
     return [(row[1], row[3]) for row in data.fetchall()]
-
-def _read_db_share_price_in_particular_day(company, date):
-    fetched_share_price_data = fetch_prices_in_particular_period(company, date, date, True)
-    return None if len(fetched_share_price_data[0]) == 0 else fetched_share_price_data[0][0]
