c326158a388af51d345142ba08539d29c397b396
Rafal Ozog <rafalozog@gmail.com>
2024-01-24 04:07:21 +0000

    Polygon code removed

--- code/config.py
+++ code/config.py
@@ -2,15 +2,6 @@ FINANCIAL_MODELING_COMPANIES_SET = [
     'TSLA', 'MSFT', 'NFLX', 'AMZN', 'GOOGL', # Tested with at least x reports
     'AAPL', 'SQ', 'NVDA', 'KO', 'VZ', # Tested with at least x reports
 ]
-POLYGON_AVAILABLE_COMPANIES_SET = [
-    'TSLA', 'MSFT', 'NFLX', 'AMZN', 'GOOGL', # Tested with at least 20 reports
-    'AAPL', 'SQ', 'NVDA', 'KO', 'VZ', # Tested with at least 20 reports
-    # 'PYPL', 'BLK', # Tested with at least 20 reports
-     # 'BRK.B', 'MRNA', 'BYND' # Issues
-]
-# NOT ENOUGH QUARTER REPORTS ON POLYGON: META (5), DIS (18 with net_cash_flow), PTON (17 with n_c_f), ALPP (6 with n_c_f), PLTR (12 with n_c_f)
-# NOT AVAILABLE ON POLYGON: SPOT, BABA, OCDO/OCDO.L, HMC, BAYN, BARC, NIO, ABEPF, XPEV
-
 
 START_DATE = '2023-01-01'
 
