1819145a92fa2d565bcf660294c92a01ee040080
Rafal Ozog <rafalozog@gmail.com>
2024-01-28 13:33:57 +0000

    Gitignore updated

--- code/main.py
+++ code/main.py
@@ -7,11 +7,11 @@ from testing_and_optimisation.optimisation_utils import run_profiler
 from testing_and_optimisation.testing_manager import run_unit_tests
 
 # FUNCTIONALITY (uncomment to run):
-run_multiple_simulations(COMPANIES_SET, START_DATE, END_DATE, SUB_PERIOD_LENGTH_IN_DAYS_ARRAY, NUMBER_OF_REPORTS_TAKEN_FOR_CALCULATION_ARRAY)
+# run_multiple_simulations(COMPANIES_SET, START_DATE, END_DATE, SUB_PERIOD_LENGTH_IN_DAYS_ARRAY, NUMBER_OF_REPORTS_TAKEN_FOR_CALCULATION_ARRAY)
 # calculate_average_market_value_per_dollar(COMPANIES_SET, START_DATE, END_DATE, SUB_PERIOD_LENGTH_IN_DAYS_ARRAY[0], NUMBER_OF_REPORTS_TAKEN_FOR_CALCULATION_ARRAY[0])
 # find_out_value_per_dollar_spent_today(COMPANIES_SET, END_DATE, 8)
 
 # DEVELOPMENT TOOLS (uncomment to run):
 # run_unit_tests()
 # read_all_data_from_database()
-# run_profiler('run_multiple_simulations(COMPANIES_SET, START_DATE, END_DATE, SUB_PERIOD_LENGTH_IN_DAYS_ARRAY, NUMBER_OF_REPORTS_TAKEN_FOR_CALCULATION_ARRAY)')
+run_profiler('run_multiple_simulations(COMPANIES_SET, START_DATE, END_DATE, SUB_PERIOD_LENGTH_IN_DAYS_ARRAY, NUMBER_OF_REPORTS_TAKEN_FOR_CALCULATION_ARRAY)')
